df713c3c62f9f7b8e2844058fa450057460321d8
Sanjana343 <sanjanagovind242@gmail.com>
2026-02-23 06:26:55 +0530

    Added semantic resume matching using sentence transformers

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, File, UploadFile
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+import numpy as np
 
 import PyPDF2
 from io import BytesIO
 
 app = FastAPI()
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # Load Job Description
 with open("job_description.txt", "r") as f:
@@ -15,21 +18,23 @@ with open("job_description.txt", "r") as f:
 async def match_resume(file: UploadFile = File(...)):
     resume_bytes = await file.read()
 
-if file.filename.endswith(".pdf"):
-    pdf_reader = PyPDF2.PdfReader(BytesIO(resume_bytes))
-    resume_text = ""
-    for page in pdf_reader.pages:
-        resume_text += page.extract_text()
+    if file.filename.endswith(".pdf"):
+        pdf_reader = PyPDF2.PdfReader(BytesIO(resume_bytes))
+        resume_text = ""
+        for page in pdf_reader.pages:
+            resume_text += page.extract_text()
 
-else:
-    resume_text = resume_bytes.decode("utf-8")
+    else:
+        resume_text = resume_bytes.decode("utf-8")
    
-    documents = [job_desc, resume_text]
+    job_embedding = model.encode(job_desc)
+    resume_embedding = model.encode(resume_text)
 
-    vectorizer = TfidfVectorizer(stop_words="english", ngram_range=(1,2))
-    tfidf_matrix = vectorizer.fit_transform(documents)
+    similarity = cosine_similarity (
+        [job_embedding],
+        [resume_embedding]
+)
 
-    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
     score = round(float(similarity[0][0]) * 100, 2)
 
     return {"Match Score (%)": score}
